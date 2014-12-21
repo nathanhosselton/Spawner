34cfe952abb18f2680f9b6eed0e7c703a5770bf5
Nathan Hosselton <nathanhosselton@gmail.com>
2014-12-21 00:41:25 -0600

    fix linker error with TimeCalc.m

---
+++ TimeCalc.m
@@ -0,0 +1,68 @@
+#import "main.h"
+
+@implementation TimeCalc
+
++ (NSTimeInterval)timeforMap:(MapIdentifier)map weapon:(WeaponIdentifier)weapon {
+    if (weapon == Rockets) {
+        switch (map) {
+            case Derelict:
+                return 30;
+            case BattleCreek:
+            case ChillOut:
+            case Damnation:
+            case HangEmHigh:
+            case Prisoner:
+                return 120;
+            case Longest:
+            case RatRace:
+            case Wizard:
+                return 0;
+        }
+    } else if (weapon == Sniper) {
+        switch (map) {
+            case BattleCreek:
+            case Damnation:
+            case Derelict:
+            case HangEmHigh:
+            case Prisoner:
+                return 30;
+            case ChillOut:
+                return 60;
+            case Longest:
+            case RatRace:
+            case Wizard:
+                return 0;
+        }
+    } else if (weapon == Overshield) {
+        switch (map) {
+            case BattleCreek:
+            case ChillOut:
+            case Damnation:
+            case Derelict:
+            case Longest:
+            case Prisoner:
+            case RatRace:
+            case Wizard:
+                return 60;
+            case HangEmHigh:
+                return 180;
+        }
+    } else if (weapon == Naked) {
+        switch (map) {
+            case BattleCreek:
+            case Damnation:
+            case Derelict:
+            case HangEmHigh:
+            case Longest:
+            case Prisoner:
+            case Wizard:
+                return 60;
+            case RatRace:
+                return 90;
+            case ChillOut:
+                return 120;
+        }
+    }
+    return 0;
+}
+@end
