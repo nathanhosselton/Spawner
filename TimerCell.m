7a05dda569d795b29948f40a1a77aaf9fe0db198
Nathan Hosselton <nathanhosselton@gmail.com>
2014-12-22 00:22:50 -0600

    TimerCell override layoutSubviews

--- TimerCell.m
+++ TimerCell.m
@@ -1,9 +1,19 @@
-//
-//  TimerCell.m
-//  Spawner
-//
-//  Created by Nathan Hosselton on 12/21/14.
-//  Copyright (c) 2014 Nathan Hosselton. All rights reserved.
-//
+#import "main.h"
+@import UIKit.UIImageView;
 
-#import <Foundation/Foundation.h>
+@implementation TimerCell
+
+- (void)layoutSubviews {
+////// image views
+    _primWpnImgView.frame = CGRectMake(0, 0, WeaponImageViewSize, WeaponImageViewSize);
+    _primWpnImgView.center = CGPointMake(_primWpnImgView.center.x, self.superview.center.y);
+
+    _subWpnImgView.frame = CGRectMake(WeaponImageViewSize+2, 0, WeaponImageViewSize, WeaponImageViewSize);
+    _subWpnImgView.center = CGPointMake(_subWpnImgView.center.x, self.superview.center.y);
+
+////// timer
+    _timerLabel.frame = CGRectMake(UIScreenWidth-100.f, 0, 100.f, 50.f);
+    _timerLabel.center = CGPointMake(_timerLabel.center.x, self.superview.center.y);
+}
+
+@end
