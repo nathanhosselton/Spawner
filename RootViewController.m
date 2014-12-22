d4786243223c26699cc6bf303a8b33a3947a6f35
Nathan Hosselton <nathanhosselton@gmail.com>
2014-12-22 00:23:23 -0600

    App showing label, button and tableview

--- RootViewController.m
+++ RootViewController.m
@@ -1,9 +1,9 @@
 #import <Foundation/Foundation.h>
 #import "main.h"
 @import UIKit.UITableView;
-@import UIKit.UITableViewCell;
-@import UIKit.UILabel;
 @import UIKit.UIButton;
+@import UIKit.UIScreen;
+@import UIKit.UITapGestureRecognizer;
 
 @interface RootViewController () <UITableViewDataSource, UITableViewDelegate>
 @end
@@ -11,40 +11,106 @@
 @implementation RootViewController {
     UILabel *mapNameLabel;
     UIButton *start;
-    UITableView *tableView;
+    UITableView *tv;
     NSMutableArray *timers;
-    MapPickerViewController *mv;
+    NSMutableArray *labels;
+    CGFloat mapLabelHeight;
+    CGFloat mapLabelWidth;
+    CGRect mapLabelFrame;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+//////
+    mapLabelHeight = 50.f;
+    mapLabelWidth = UIScreenWidth*(2.f/3.f);
+    CGFloat lx = (UIScreenWidth - mapLabelWidth)/2;
+    mapLabelFrame = CGRectMake(lx, mapLabelHeight, mapLabelWidth, mapLabelHeight);
+
+////// button
     [self.view addSubview:start = [UIButton buttonWithType:UIButtonTypeCustom]];
+    [start setFrame:CGRectMake(0, mapLabelHeight*3, 100.f, 50.f)];
+    [start setCenter:CGPointMake(self.view.center.x, start.center.y)];
     [start setTitle:@"Start" forState:UIControlStateNormal];
     [start setTitle:@"Stop" forState:UIControlStateSelected];
     [start addTarget:self action:@selector(onstart:) forControlEvents:UIControlEventTouchUpInside];
 
-    [self.view addSubview:tableView = [UITableView new]];
+////// map picker
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(ontap:)];
+    NSArray *mapNames = [NSArray arrayWithObjects: @"Battle Creek",
+                                                   @"Chill Out",
+                                                   @"Damnation",
+                                                   @"Derelict",
+                                                   @"Hang 'Em High",
+                                                   @"Longest",
+                                                   @"Prisoner",
+                                                   @"Rat Race",
+                                                   @"Wizard", nil];
+    labels = [NSMutableArray arrayWithCapacity:mapNames.count];
+    for (NSString *map in mapNames) {
+        UILabel *label = [[UILabel alloc] initWithFrame:mapLabelFrame];
+        [label setCenter:CGPointMake(self.view.center.x, label.center.y)];
+        label.text = map;
+        label.tag = [mapNames indexOfObject:map];
+        [label setTextAlignment:NSTextAlignmentCenter];
+        [label setBackgroundColor:[self colorForMapIndex:label.tag]];
+        label.userInteractionEnabled = YES;
+        [label addGestureRecognizer:tap];
 
-    mv = [MapPickerViewController new];
-    mv.mapNames = [NSArray arrayWithObjects: @"Battle Creek",
-                   @"Chill Out",
-                   @"Damnation",
-                   @"Derelict",
-                   @"Hang 'Em High",
-                   @"Longest",
-                   @"Prisoner",
-                   @"Rat Race",
-                   @"Wizard", nil];
+        [self.view addSubview:label];
+        [labels addObject:label];
+    }
+    self.currentMap = ((UILabel *)labels.firstObject).tag;
 
-    [self.view addSubview:mapNameLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 44.f)]];
-    mapNameLabel.text = mv.mapNames.firstObject;
-    mapNameLabel.userInteractionEnabled = YES;
+////// table
+    CGFloat ty = mapLabelHeight*5;
+    CGRect trect = CGRectMake(0, ty, UIScreenWidth, UIScreenHeight-ty);
+    tv = [[UITableView alloc] initWithFrame:trect style:UITableViewStylePlain];
+    [tv setBackgroundView:nil];
+    [tv setBackgroundColor:[UIColor clearColor]];
+    [tv registerClass:[TimerCell class] forCellReuseIdentifier:NSStringFromClass(TimerCell.class)];
+    [self.view addSubview:tv];
+    tv.delegate = self;
+    tv.dataSource = self;
 
+//////
     timers = [NSMutableArray arrayWithCapacity:4];
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return timers.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    TimerCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass(TimerCell.class) forIndexPath:indexPath];
+
+    cell.thetimer = [timers objectAtIndex:indexPath.row];
+    [cell setBackgroundView:nil];
+    [cell setBackgroundColor:[self colorForMapIndex:_currentMap]];
+    //TODO: Create weapon images
+    [cell layoutSubviews];
+
+    return cell;
+}
+
+- (UIColor *)colorForMapIndex:(MapIdentifier)mapIndex {
+    switch (mapIndex) {
+        case BattleCreek:
+        case ChillOut:
+        case Damnation:
+        case Derelict:
+        case HangEmHigh:
+        case Longest:
+        case Prisoner:
+        case RatRace:
+        case Wizard: return [UIColor colorWithRed:0.895 green:0.903 blue:0.657 alpha:1.000];
+    }
+    return [UIColor clearColor];
+}
+
 - (void)onstart:(UIButton *)button {
+    NSLog(@"%u", button.state);
     if (button.state == UIControlStateNormal) {
         // Start timers
 
@@ -56,18 +122,8 @@
     }
 }
 
-@end
-
-@implementation MapPickerViewController {
-    NSMutableArray *labels;
-}
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-
-    labels = [NSMutableArray arrayWithCapacity:self.mapNames.count];
-    for (NSString *map in self.mapNames)
-        [labels addObject:[UILabel new].text = map];
+- (void)ontap:(UILabel *)label {
+    NSLog(@"Tapped");
 }
 
 @end
